a6fe9938e4cd11048a6d40dfdab6468b953b16f2
home1207 <hellowgw@gmail.com>
2016-09-08 00:07:44 +0800

    优化代码包上传

--- app01/views/comm.py
+++ app01/views/comm.py
@@ -22,12 +22,39 @@ def get_production_info(request):
 
 
 def test(request):
-    x = handle_path(3, 2, 28)
+    x = handle_local_path(3, 2, 28)
     print(x)
     return HttpResponse('ok')
 
 
-def handle_path(pushtarget_id, prod_line_id, prod_id):
+def check_select(request):
+    info_dic = {}
+    success_list = []
+    error_list = []
+    push_target_id = request.POST['PushTarget']
+    production_line_id = request.POST['productions-line']
+    production_id = request.POST['production']
+    if int(push_target_id) == 1:
+        error_list.append('未选择发布环境')
+    else:
+        success_list.append('已选择发布环境')
+    if not production_line_id.isdigit():
+        error_list.append('未选择产品线')
+    else:
+        success_list.append('已选择产品线')
+    if not production_id.isdigit():
+        error_list.append('未选择产品')
+    else:
+        success_list.append('已选择产品')
+    info_dic['error'] = error_list
+    info_dic['success'] = success_list
+    info_dic['pt_id'] = push_target_id
+    info_dic['pl_id'] = production_line_id
+    info_dic['p_id'] = production_id
+    return info_dic
+
+
+def handle_local_path(pushtarget_id, prod_line_id, prod_id):
     push_name = models.PushTarget.objects.get(id=pushtarget_id).target_name
     prod_line_name = models.ProductionLine.objects.get(id=prod_line_id).pl_name
     prod_name = models.Production.objects.get(id=prod_id).p_name
--- app01/views/package.py
+++ app01/views/package.py
@@ -2,34 +2,31 @@
 # coding:utf-8
 from django.shortcuts import render
 from app01.forms.main_form import PackageForm
+from app01.views.comm import handle_local_path
+from app01.views.comm import check_select
 
 
 def upload_package(request):
     upload_obj = PackageForm()
     if request.method == 'POST':
-        push_target_id = request.POST['PushTarget']
-        production_line_id = request.POST['productions-line']
-        production_id = request.POST['production']
+        msg_dic = check_select(request)
+        success_msg_list = msg_dic['success']
+        err_msg_list = msg_dic['error']
         upload_file_obj = request.FILES.get('upload_file')
-        success_msg_list = []
-        err_msg_list = []
-        if int(push_target_id) == 1:
-            err_msg_list.append('没有选择发布环境')
-        if not production_line_id.isdigit():
-            err_msg_list.append('没有选择产品线')
-        if not production_id.isdigit():
-            err_msg_list.append('没有选择产品')
+
         if not upload_file_obj:
-            err_msg_list.append('没有指定上传数据')
+            err_msg_list.append('未来指定数据包')
+        else:
+            success_msg_list.append('已指定数据包')
         if err_msg_list:
             return render(request, 'main/package.html', {'upload': upload_obj, 'err_msg': err_msg_list})
         else:
-            success_msg_list.append('上传条件满足')
-            file_name = upload_file_obj.name
-            upload_path = 'upload/%s' % file_name
+            # upload_file_size = upload_file_obj.size
+            upload_path = handle_local_path(int(msg_dic['pt_id']), int(msg_dic['pl_id']), int(msg_dic['p_id']))
             with open(upload_path, 'wb') as f:
                 for data in upload_file_obj.chunks():
                     f.write(data)
+            success_msg_list.append('上传代码包成功')
             return render(request, 'main/package.html', {'upload': upload_obj, 'success_msg': success_msg_list})
 
     return render(request, 'main/package.html', {'upload': upload_obj})
